06f2b811bad7f9deb0255525227f0cc3e86cc723
Christopher Diaz <crdiaz3@asu.edu>
2016-12-20 17:06:15 -0700

    adjusted whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_filter :protect
 
   def protect
-    @ips = ['209.147.144.0/24'] #And so on ...]
+    @ips = ['68.3.149.151'] #And so on ...]
     allowed = false
     # Convert remote IP to an integer.
     bremote_ip = request.remote_ip.split('.').map(&:to_i).pack('C*').unpack('N').first
