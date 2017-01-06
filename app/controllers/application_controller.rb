b3bfd7d75c80e6231a067f66e205a04cac5989d4
Christopher Diaz <crdiaz3@asu.edu>
2017-01-05 21:19:28 -0700

    gem updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :protect
-
-  def protect
-    @ips = ['68.3.149.151'] #And so on ...]
-    allowed = false
-    # Convert remote IP to an integer.
-    bremote_ip = request.remote_ip.split('.').map(&:to_i).pack('C*').unpack('N').first
-    @ips.each do |ipstring|
-      ip, mask = ipstring.split '/'
-      # Convert tested IP to an integer.
-      bip = ip.split('.').map(&:to_i).pack('C*').unpack('N').first
-      # Convert mask to an integer, and assume /32 if not specified.
-      mask = mask ? mask.to_i : 32
-      bmask = ((1 << mask) - 1) << (32 - mask)
-      if bip & bmask == bremote_ip & bmask
-        allowed = true
-        break
-      end
-    end
-
-    if not allowed
-       render :text => "You are unauthorized"
-       return
-    end
-  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160125042300) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,9 +35,8 @@ ActiveRecord::Schema.define(version: 20160125042300) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
