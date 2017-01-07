ba2b5dcb48f01c8eadbcad04c640b6309cc430fb
Christopher Diaz <crdiaz3@asu.edu>
2017-01-07 12:39:30 -0700

    tinymce added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add to settle tinymce issue
+  config.assets.debug = true
 end
